a5ac37f38d7a2a8091d20f94e9d56c5beec5339c
Eric Harney <eharney@redhat.com>
2025-07-15 20:54:03 +0000

    Note about threading Semaphore
    Change-Id: I76f79fc6f3b07a39aba95c06ac84bf78c9f2250c

--- cinder/utils.py
+++ cinder/utils.py
@@ -1005,6 +1005,7 @@ def create_ordereddict(adict: dict) -> OrderedDict:
 
 class Semaphore(object):
     """Custom semaphore to workaround eventlet issues with multiprocessing."""
+    # TODO (eventlet-removal) : replace with threading.Semaphore implementation
     def __init__(self, limit):
         self.limit = limit
         self.semaphore = multiprocessing.Semaphore(limit)
